7f1cc6750ca55e1537ca0e1d6fd777f5aae3e406
Ceonnn <68862382+Ceonnn@users.noreply.github.com>
2022-01-24 21:01:31 +0300

    Update veri.js

--- komutlar/veri.js
+++ komutlar/veri.js
@@ -5,7 +5,7 @@ const db = require('quick.db')
 exports.run = async (client, message, args) => {
 if(!ayarlar.sahip.includes(message.author.id)) return message.reply(new Discord.MessageEmbed().setDescription('Sen sahibim değilsin!')).then(Hata => Hata.delete({timeout:15000}))
 
-if (args[0] == 'acekle') {
+if (args[0] == 'coinekle') {
 const Üye = message.mentions.users.first() || client.users.cache.get(args[1])
 if (!Üye) return message.reply('Bir üye etiketleyin.')
 const Miktar = args[1]
@@ -13,11 +13,11 @@ if (isNaN(args[1])) return message.reply('Lütfen miktar yazın!')
 message.reply(`İşlem tamamlandı!`)
 db.add(`Puan_${Üye.id}`,Miktar)
 } else {
-if (args[0] == 'acsil') {
+if (args[0] == 'coinsil') {
   const Üye = message.mentions.users.first() || client.users.cache.get(args[1])
   if (!Üye) return message.reply('Bir üye etiketleyin.')
-  const Miktar = args[2]
-  if (isNaN(args[2])) return message.reply('Lütfen miktar yazın!')
+  const Miktar = args[1]
+  if (isNaN(args[1])) return message.reply('Lütfen miktar yazın!')
 message.reply(`İşlem tamamlandı!`)
   db.add(`Puan_${Üye.id}`,-Miktar)
 } else {
@@ -26,7 +26,7 @@ if (args[0] == 'hakkımdasil') {
   if (!Üye) return message.reply('Bir üye etiketleyin.')
 message.reply(`İşlem tamamlandı!`)
   db.delete(`hakkimda_${Üye.id}`)
-} else return message.reply('Seçenek girin. `acekle` | `acsil` | `hakkımdasil`')
+} else return message.reply('Seçenek girin. `coinekle` | `coinsil` | `hakkımdasil`')
 }
 } 
 }
